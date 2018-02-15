5b804b780ebaee01294568237e64062a85e150fd
Anton Skorik <anton.skorik@cbsi.com>
2018-02-15 16:50:10 +0300

    Added option to enable Compile include nodes generation. It is off by default and <EnableDefaultCompileItems>False</EnableDefaultCompileItems> node is generated

--- Project2015To2017/Settings.cs
+++ Project2015To2017/Settings.cs
@@ -8,11 +8,54 @@ namespace Project2015To2017
 {
 	public class Settings
 	{
+		/// <summary>
+		/// Gets or sets a value indicating whether TargetFramework node should change its value from existing to netstandard2.0.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if TargetFramework node should change its value from existing to netstandard2.0; otherwise, <c>false</c>.
+		/// </value>
 		public bool IsMigrateToNetStandard { set; get; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether the AssemblyInfo file should be used instead of palcing all the information from it to *.csproj.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance is use assembly information file; otherwise, <c>false</c>.
+		/// </value>
+		/// <remarks>This results in Generate*Attribute nodes generation with default false values. AssemblyInfo file is not deleted and still used.</remarks>
 		public bool IsUseAssemblyInfoFile { set; get; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether VS PSCC TFS configuration file should be used to configure version control.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if VS PSCC TFS configuration file should be used to configure version control; otherwise, <c>false</c>.
+		/// </value>
+		/// <remarks>This results in *Scc nodes with SAK default value generation </remarks>
 		public bool IsUseVsPsccFileToConfigureVersionControl { set; get; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether *.old files should be deleted. This option does not delete *.csproj.old file.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance is delete old files except project; otherwise, <c>false</c>.
+		/// </value>
 		public bool IsDeleteOldFilesExceptProject { set; get; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether the TFS settings file should be deleted.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if the TFS settings file should be deleted; otherwise, <c>false</c>.
+		/// </value>
 		public bool IsDeleteTfsSettingsFile { set; get; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether the Compile items definitions from old project file should be automatically generated instead of being explicitly manually included as-is.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if Compile items definitions from old project file should be automatically generated instead of being explicitly manually included as-is.; otherwise, <c>false</c>.
+		/// </value>
 		public bool IsEnableDefaultCompileItems { set; get; }
 
 		/// <summary>
--- Project2015To2017/Transformations/FileTransformation.cs
+++ Project2015To2017/Transformations/FileTransformation.cs
@@ -8,120 +8,122 @@ using Project2015To2017.Definition;
 
 namespace Project2015To2017.Transformations
 {
-    internal sealed class FileTransformation : ITransformation
-    {
-        private static readonly IReadOnlyList<string> ItemsToProject = new[]
-        {
-            "None",
-            "Content",
-            "AdditionalFiles",
-            "CodeAnalysisDictionary",
-            "ApplicationDefinition",
-            "Page",
-            "Resource",
-            "SplashScreen",
-            "DesignData",
-            "DesignDataWithDesignTimeCreatableTypes",
-            "EntityDeploy",
-            "XamlAppDef"
-        };
+	internal sealed class FileTransformation : ITransformation
+	{
+		private static readonly IReadOnlyList<string> ItemsToProject = new[]
+		{
+			"None",
+			"Content",
+			"AdditionalFiles",
+			"CodeAnalysisDictionary",
+			"ApplicationDefinition",
+			"Page",
+			"Resource",
+			"SplashScreen",
+			"DesignData",
+			"DesignDataWithDesignTimeCreatableTypes",
+			"EntityDeploy",
+			"XamlAppDef"
+		};
 
-        public Task TransformAsync(XDocument projectFile, DirectoryInfo projectFolder, Project definition, Settings settings)
-        {
-            XNamespace nsSys = "http://schemas.microsoft.com/developer/msbuild/2003";
-            var itemGroups = projectFile
-                .Element(nsSys + "Project")
-                .Elements(nsSys + "ItemGroup");
+		public Task TransformAsync(XDocument projectFile, DirectoryInfo projectFolder, Project definition, Settings settings)
+		{
+			XNamespace nsSys = "http://schemas.microsoft.com/developer/msbuild/2003";
+			var itemGroups = projectFile
+				.Element(nsSys + "Project")
+				.Elements(nsSys + "ItemGroup");
 
-            var compileManualIncludes = FindNonWildcardMatchedFiles(projectFolder, itemGroups, "*.cs", nsSys + "Compile");
-            var otherIncludes = ItemsToProject.SelectMany(x => itemGroups.Elements(nsSys + x));
+			var compileManualIncludes = FindNonWildcardMatchedFiles(projectFolder, itemGroups, "*.cs", nsSys + "Compile");
+			var otherIncludes = ItemsToProject.SelectMany(x => itemGroups.Elements(nsSys + x));
 
-            // Remove packages.config since those references were already added to the CSProj file.
-            otherIncludes.Where(x => x.Attribute("Include")?.Value == "packages.config").Remove();
+			// Remove packages.config since those references were already added to the CSProj file.
+			otherIncludes.Where(x => x.Attribute("Include")?.Value == "packages.config").Remove();
 
-            definition.ItemsToInclude = compileManualIncludes.Concat(otherIncludes).ToArray();
+			definition.ItemsToInclude = settings.IsEnableDefaultCompileItems
+				? otherIncludes.ToArray()
+				: compileManualIncludes.Concat(otherIncludes).ToArray();
+			
+			return Task.CompletedTask;
+		}
 
-            return Task.CompletedTask;
-        }
+		private static IReadOnlyList<XElement> FindNonWildcardMatchedFiles(
+			DirectoryInfo projectFolder,
+			IEnumerable<XElement> itemGroups,
+			string wildcard,
+			XName elementName)
+		{
+			var manualIncludes = new List<XElement>();
+			var filesMatchingWildcard = new List<string>();
+			foreach (var compiledFile in itemGroups.Elements(elementName))
+			{
+				var includeAttribute = compiledFile.Attribute("Include");
+				if (includeAttribute != null && !includeAttribute.Value.Contains("*"))
+				{
+					if (!Path.GetFullPath(Path.Combine(projectFolder.FullName, includeAttribute.Value)).StartsWith(projectFolder.FullName))
+					{
+						Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile}.");
+						manualIncludes.Add(compiledFile);
+					}
+					else if (compiledFile.Attributes().Count() != 1)
+					{
+						Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile}.");
+						manualIncludes.Add(compiledFile);
+					}
+					else if (compiledFile.Elements().Count() != 0)
+					{
+						var dependentUpon = compiledFile.Element(elementName.Namespace + "DependentUpon");
+						if (dependentUpon != null && dependentUpon.Value.EndsWith(".resx", StringComparison.OrdinalIgnoreCase))
+						{
+							// resx generated code file
+							manualIncludes.Add(new XElement(
+								"Compile",
+								new XAttribute("Update", includeAttribute.Value),
+								new XElement("DependentUpon", dependentUpon.Value)));
 
-        private static IReadOnlyList<XElement> FindNonWildcardMatchedFiles(
-            DirectoryInfo projectFolder,
-            IEnumerable<XElement> itemGroups,
-            string wildcard,
-            XName elementName)
-        {
-            var manualIncludes = new List<XElement>();
-            var filesMatchingWildcard = new List<string>();
-            foreach (var compiledFile in itemGroups.Elements(elementName))
-            {
-                var includeAttribute = compiledFile.Attribute("Include");
-                if (includeAttribute != null && !includeAttribute.Value.Contains("*"))
-                {
-                    if (!Path.GetFullPath(Path.Combine(projectFolder.FullName, includeAttribute.Value)).StartsWith(projectFolder.FullName))
-                    {
-                        Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile}.");
-                        manualIncludes.Add(compiledFile);
-                    }
-                    else if (compiledFile.Attributes().Count() != 1)
-                    {
-                        Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile}.");
-                        manualIncludes.Add(compiledFile);
-                    }
-                    else if (compiledFile.Elements().Count() != 0)
-                    {
-                        var dependentUpon = compiledFile.Element(elementName.Namespace + "DependentUpon");
-                        if (dependentUpon != null && dependentUpon.Value.EndsWith(".resx", StringComparison.OrdinalIgnoreCase))
-                        {
-                            // resx generated code file
-                            manualIncludes.Add(new XElement(
-                                "Compile",
-                                new XAttribute("Update", includeAttribute.Value),
-                                new XElement("DependentUpon", dependentUpon.Value)));
+							filesMatchingWildcard.Add(includeAttribute.Value);
+						}
+						else
+						{
+							Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile}.");
+							manualIncludes.Add(compiledFile);
+						}
+					}
+					else
+					{
+						filesMatchingWildcard.Add(includeAttribute.Value);
+					}
+				}
+				else
+				{
+					Console.WriteLine($"Compile found with no or wildcard include, full node {compiledFile}.");
+				}
+			}
 
-                            filesMatchingWildcard.Add(includeAttribute.Value);
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Include cannot be done through wildcard, adding as separate include {compiledFile}.");
-                            manualIncludes.Add(compiledFile);
-                        }
-                    }
-                    else
-                    {
-                        filesMatchingWildcard.Add(includeAttribute.Value);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Compile found with no or wildcard include, full node {compiledFile}.");
-                }
-            }
+			var filesInFolder = projectFolder.EnumerateFiles(wildcard, SearchOption.AllDirectories).Select(x => x.FullName).ToArray();
+			var knownFullPaths = manualIncludes
+				.Select(x => x.Attribute("Include")?.Value)
+				.Where(x => x != null)
+				.Concat(filesMatchingWildcard)
+				.Select(x => Path.GetFullPath(Path.Combine(projectFolder.FullName, x)))
+				.ToArray();
 
-            var filesInFolder = projectFolder.EnumerateFiles(wildcard, SearchOption.AllDirectories).Select(x => x.FullName).ToArray();
-            var knownFullPaths = manualIncludes
-                .Select(x => x.Attribute("Include")?.Value)
-                .Where(x => x != null)
-                .Concat(filesMatchingWildcard)
-                .Select(x => Path.GetFullPath(Path.Combine(projectFolder.FullName, x)))
-                .ToArray();
+			foreach (var nonListedFile in filesInFolder.Except(knownFullPaths))
+			{
+				if (nonListedFile.StartsWith(Path.Combine(projectFolder.FullName + "\\obj"), StringComparison.OrdinalIgnoreCase))
+				{
+					// skip the generated files in obj
+					continue;
+				}
 
-            foreach (var nonListedFile in filesInFolder.Except(knownFullPaths))
-            {
-                if (nonListedFile.StartsWith(Path.Combine(projectFolder.FullName + "\\obj"), StringComparison.OrdinalIgnoreCase))
-                {
-                    // skip the generated files in obj
-                    continue;
-                }
+				Console.WriteLine($"File found which was not included, consider removing {nonListedFile}.");
+			}
 
-                Console.WriteLine($"File found which was not included, consider removing {nonListedFile}.");
-            }
+			foreach (var fileNotOnDisk in knownFullPaths.Except(filesInFolder).Where(x => x.StartsWith(projectFolder.FullName, StringComparison.OrdinalIgnoreCase)))
+			{
+				Console.WriteLine($"File was included but is not on disk: {fileNotOnDisk}.");
+			}
 
-            foreach (var fileNotOnDisk in knownFullPaths.Except(filesInFolder).Where(x => x.StartsWith(projectFolder.FullName, StringComparison.OrdinalIgnoreCase)))
-            {
-                Console.WriteLine($"File was included but is not on disk: {fileNotOnDisk}.");
-            }
-
-            return manualIncludes;
-        }
-    }
+			return manualIncludes;
+		}
+	}
 }
